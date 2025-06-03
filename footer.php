bb68494c9bcf0765ad9c6907f39e2ac090321e18
Dava <cdava123@gmail.com>
2025-06-03 12:09:29 +0700

    update database pemrograman_web_contoh

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <p>&copy; 2025 halaman web dafa Alfauzi. dibuat dengan css</p>
+    </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include 'header.php'; ?>
+
+<div class="container">
+    <section class="content">
+        <h2>artikel utama</h2>
+        <p>ini adalah contoh halaman web sederhana menggunakan css dan flexbox untuk tata letak.</p>
+        <button class="btn">klik saya</button>
+    </section>
+    <aside class="sidebar">
+        <h2>Link pertemuan sebelumnya</h2>
+        <br>
+        <h4>
+            <p>pertemuan 3</p>
+            <p><a href="praktikum/p3.html">html </a><a href="tugas/tugas3.html">tugas </a></p>
+            <br>
+            <p>pertemuan 4</p>
+            <p><a href="praktikum/p3.html">html </a><a href="tugas/p3.html">tugas </a></p>
+        </h4>
+    </aside>
+</div>
+
+<?php include 'footer.php'; ?>
